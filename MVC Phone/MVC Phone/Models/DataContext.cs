3ecf6c04fdc46d91e9271bc82ea1b0949297e19a
faltamirano98@hotmail.com <faltamirano98@hotmail.com>
2019-08-24 13:45:09 -0400

    Solucion Completa

---
+++ MVC Phone/MVC Phone/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC_Phone.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVC_Phone.Models.Phone> Phones { get; set; }
+    }
+}
---
+++ MVC Phone/MVC Phone/Models/Phone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Phone.Models
+{
+    public enum TypeContact
+    {
+        PhoneNumber,
+        Email,
+        Facebook,
+        Twitter,
+        LinkedIN
+    }
+    public class Phone
+    {
+        [Key]
+        public int PhoneID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Ty { get; set; }
+        [Required]
+        public string Contact { get; set; }
+    }
+}
